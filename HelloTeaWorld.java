7f5a477423d8d1bd815535f1a1f9bd0f821ec74d
Jürgen Smolka <30924797+ttimer@users.noreply.github.com>
2021-07-31 15:54:41 +0200

    Add files via upload

---
+++ HelloTeaWorld.java
@@ -0,0 +1,19 @@
+// ************************************************** 
+//     File:	aTeaTimer4U.java
+//     Project:	a-Tea-Timer-4-U 
+//     Content:	Main part of the timer
+//     Copyr.©:	ITS SARL -- www.jes.it
+//              http://smolka.lima-city.de/
+// ************************************************** 
+
+package it.jes.timer;
+
+public class HelloTeaWorld {
+
+	/* @param args */
+	public static void main(String[] args) {
+		System.out.println("Hello Tea World !");
+		System.out.println("Here is a-Tea-Timer-4-U.");
+	}
+
+}
